9da05decf8efca94c7b5fdacd28671de7c1ffc42
Charles Dickens <charlieadickens@gmail.com>
2021-05-14 16:04:35 -0700

    Distinguish between x and x_slack in lcqp.

--- smooth_psl_optimization_example.m
+++ smooth_psl_optimization_example.m
@@ -8,6 +8,8 @@ example_name = "epinions";
     linear_potentials, quadratic_potentials, ...
     constraints, meta_data] = load_example_data(example_name);
 
+%% Initialize decision variables randomly in (0, 1).
+x0 = rand(meta_data.Num_Variables, 1);
 
 %% Example Smooth Problem Optimization.
 % Define QP.
@@ -15,4 +17,6 @@ options = optimoptions('quadprog','Display','iter');
 [H, f, A, b, Aeq, beq, lb, ub] = get_qp_problem(hinge_potentials, ...
     square_hinge_potentials, linear_potentials, quadratic_potentials, ...
     constraints, meta_data);
-x = quadprog(H,f,A,b,Aeq,beq,lb,ub,x0,options);
+x_smooth = quadprog(H,f,A,b,Aeq,beq,lb,ub,x0,options);
+x = x_smooth(1:meta_data.Num_Variables);
+x_slack = x_smooth(meta_data.Num_Variables : end);
