a1989bc06c2744027d550884414ed43c012781a4
Charles Dickens <charlieadickens@gmail.com>
2021-06-23 09:31:02 -0700

    LastFM smooth example fix.

--- smooth_helpers/get_qp_problem.m
+++ smooth_helpers/get_qp_problem.m
@@ -5,7 +5,7 @@ function [H, f, A, b, Aeq, beq, lb, ub] = get_qp_problem(hinge_potentials, ...
     smooth_num_variables = meta_data.Num_Variables + height(square_hinge_potentials) + height(hinge_potentials);
 
     % Quadratic Potentials.
-    Q_Q = sparse(height(quadratic_potentials), smooth_num_variables);
+    Q_Q = sparse(meta_data.Num_Variables, meta_data.Num_Variables);
     b_Q = sparse(smooth_num_variables, 1);
     for i = 1: height(quadratic_potentials)
         var_indices = quadratic_potentials.Var_Index{i}{1};
@@ -30,21 +30,16 @@ function [H, f, A, b, Aeq, beq, lb, ub] = get_qp_problem(hinge_potentials, ...
     end
 
     % Square Hinge Potentials.
-    Q_SH = sparse(height(square_hinge_potentials), smooth_num_variables);
-    A_SH = sparse(height(square_hinge_potentials), smooth_num_variables);
-    c_SH = sparse(height(square_hinge_potentials), 1);
+    Q_SH = spdiags(square_hinge_potentials.Weight, 0, height(square_hinge_potentials), height(square_hinge_potentials));
+    A_SH = spdiags(-1 * ones(height(square_hinge_potentials), 1), meta_data.Num_Variables, height(square_hinge_potentials), smooth_num_variables);
+    c_SH = sparse(square_hinge_potentials.Constant);
     for i = 1: height(square_hinge_potentials)
         var_indices = square_hinge_potentials.Var_Index{i}{1};
         var_coefficients = square_hinge_potentials.Var_Coefficient{i}{1};
-        constant = square_hinge_potentials.Constant(i);
-        weight = square_hinge_potentials.Weight(i);
 
-        Q_SH(i, meta_data.Num_Variables + i) = weight;
         A_SH(i, var_indices) = var_coefficients;
-        A_SH(i, meta_data.Num_Variables + i) = -1;
-        c_SH(i) = constant;
     end
-
+    
     % Linear Hinge Potentials.
     b_H = sparse(smooth_num_variables, 1);
     A_H = sparse(height(hinge_potentials), smooth_num_variables);
@@ -73,29 +68,14 @@ function [H, f, A, b, Aeq, beq, lb, ub] = get_qp_problem(hinge_potentials, ...
         c_C(i) = constant;
     end
 
-    % Box constraints on x.
-    A_box = sparse(meta_data.Num_Variables * 2, smooth_num_variables);
-    c_box = [ones(meta_data.Num_Variables, 1); zeros(meta_data.Num_Variables, 1)];
-    for i = 1: (meta_data.Num_Variables)
-        A_box(i, i) = 1;
-        A_box(i + meta_data.Num_Variables, i) = -1;
-    end
-
-    % Slack Potentials.
-    A_slack = sparse(height(hinge_potentials) + height(square_hinge_potentials), smooth_num_variables);
-    c_slack = zeros((height(hinge_potentials) + height(square_hinge_potentials)), 1);
-    for i = 1: (height(hinge_potentials) + height(square_hinge_potentials))
-        A_slack(i, meta_data.Num_Variables + i) = -1;
-    end
-
     % Define QP.
-    H = [Q_Q;Q_SH]; 
+    H = [Q_Q, sparse(height(Q_Q), width(Q_SH)); sparse(height(Q_SH), width(Q_Q)), Q_SH]; 
     f = b_Q + b_L + b_H;
-    A = [A_SH; A_H; A_C; A_box; A_slack];
-    b = [c_SH; c_H; c_C; c_box; c_slack];
+    A = [A_SH; A_H; A_C];
+    b = [c_SH; c_H; c_C];
     Aeq = [];
     beq = [];
-    lb = [];
-    ub = [];
+    lb = zeros(smooth_num_variables, 1);
+    ub = [ones(meta_data.Num_Variables, 1); inf(height(hinge_potentials) + height(square_hinge_potentials), 1)];
 end
 
--- smooth_psl_optimization_example.m
+++ smooth_psl_optimization_example.m
@@ -2,7 +2,7 @@
 addpath("./loading_functions")
 addpath("./smooth_helpers")
 
-example_name = "cora";
+example_name = "epinions";
 
 [hinge_potentials, square_hinge_potentials, ...
     linear_potentials, quadratic_potentials, ...
@@ -19,6 +19,7 @@ options = optimoptions('quadprog','Display','iter-detailed', 'Diagnostics', 'on'
 [H, f, A, b, Aeq, beq, lb, ub] = get_qp_problem(hinge_potentials, ...
     square_hinge_potentials, linear_potentials, quadratic_potentials, ...
     constraints, meta_data);
+%%
 [x_smooth, fval, exitflag, output, lambda] = quadprog(H,f,A,b,Aeq,beq,lb,ub,x0,options);
 x = x_smooth(1:meta_data.Num_Variables);
 x_slack = x_smooth(meta_data.Num_Variables : end);
@@ -29,6 +30,9 @@ fval = [7.306427e+03, 9.459660e+03, 8.484374e+03, 2.258432e+03, ...
     9.099660e+02, 5.724310e+02, 4.794913e+02, 4.390656e+02, 4.284131e+02, ...
     4.231970e+02, 4.213697e+02, 4.200841e+02, 4.199690e+02];
 plot(t, fval, 'Marker', 'd', 'Color', 'm', 'MarkerFaceColor', 'm', 'LineStyle', ':')
+title('Objective Value')
+xlabel('Epochs')
+ylabel('Objective Value')
 
 %% Adhoc Plot of results (Hardcoded Cora Results)
 t = linspace(0,15,16);
@@ -36,6 +40,16 @@ fval = [1.512841e+04, 1.521760e+04, 1.620258e+04, 1.528806e+04, ...
     8.483221e+03, 3.639331e+03, 1.521432e+03, 6.934081e+02, 3.584906e+02, ...
     2.287068e+02, 1.956227e+02, 1.860223e+02, 1.837776e+02, 1.832569e+02, ...
     1.831800e+02, 1.831430e+02];
+plot(t, fval, 'Marker', 'd', 'Color', 'm', 'MarkerFaceColor', 'm', 'LineStyle', ':')
+title('Objective Value')
+xlabel('Epochs')
+ylabel('Objective Value')
+set(gca, 'YScale', 'log')
+
+%% Adhoc Plot of results (Hardcoded LastFM Results)
+t = linspace(0,8,9);
+fval = [7.007025e+05, 3.643277e+07, 3.248020e+07, 5.420902e+06, ...
+    8.460064e+05, 2.421636e+05, 1.094466e+05, 3.095473e+04, 1.385122e+04];
 plot(t, fval, 'Marker', 'd', 'Color', 'm', 'MarkerFaceColor', 'm', 'LineStyle', ':')
 title('Objective Value')
 xlabel('Epochs')
